72d379223aca43a9eb9f3e9542790496000d6317
shawn25n92 <shawn25n92@gmail.com>
2017-10-25 14:45:42 +0800

    Initial commit

--- SQLBrowser2/MainWindow.xaml.cs
+++ SQLBrowser2/MainWindow.xaml.cs
@@ -24,5 +24,10 @@ namespace SQLBrowser2
         {
             InitializeComponent();
         }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("Hello World");
+        }
     }
 }
